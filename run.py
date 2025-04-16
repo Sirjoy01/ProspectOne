7c284d88c12e8fda2f3552882b6e7bbdd91ee606
Sirjoy01 <chamed850@gmail.com>
2025-04-17 01:39:26 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
-python run.py
+from flask import Flask
 from app.routes import *
 from utils.database import init_db
 
-init_db()
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    init_db()
+    app.run(host="0.0.0.0", port=5000)
