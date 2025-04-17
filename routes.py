60286ee96803a216ceae9bcf2f09434080eaec64
Sirjoy01 <chamed850@gmail.com>
2025-04-17 14:56:13 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,27 +1,10 @@
-from flask import render_template, request, redirect, url_for, session
+from flask import render_template
 from run import app
-from database import validate_login, get_suggestions, save_contacted, get_user_preferences
-from messaging import prepare_message
 
-@app.route("/", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        distributor = request.form["distributor"]
-        if validate_login(distributor):
-            session["distributor"] = distributor
-            return redirect(url_for("home"))
-        else:
-            return render_template("login.html", error="Accès refusé.")
-    return render_template("login.html")
-@app.route("/home", methods=["GET", "POST"])
-def home():
-    if "distributor" not in session:
-        return redirect(url_for("login"))
-    distributor = session["distributor"]
-    preferences = get_user_preferences(distributor)
-    suggestions = get_suggestions(distributor, preferences)
-    return render_template("home.html", suggestions=suggestions)
+@app.route("/")
+def index():
+    return "Index fonctionne bien !"
 
 @app.route("/test")
 def test():
-    return "ça marche !"
+    return "Test fonctionne bien !"
