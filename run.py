3542b4f123b67d43e6b899fdca387599aa75a356
Sirjoy01 <chamed850@gmail.com>
2025-04-17 02:20:30 +0200

    Update run.py

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ from flask import Flask
 from routes import *
 from database import init_db
 
-flask_app = Flask(__name__)
 app = flask_app
+flask_app = Flask(__name__)
 
 if __name__ == "__main__":
     init_db()
