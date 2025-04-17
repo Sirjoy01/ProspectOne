862e3ea69c6983c73f75ea33ab2a33ee24ef2d75
Sirjoy01 <chamed850@gmail.com>
2025-04-17 02:02:46 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 from flask import Flask
-from app.routes import *
+from routes import *
 from utils.database import init_db
 
-app = Flask(_ _name_ _)
+app = Flask(__name__)
 
-if _ _name_ _ == "_ _main_ _":
+if __name__ == "__main__":
     init_db()
     app.run(host="0.0.0.0", port=5000)
-
 
 app = app
