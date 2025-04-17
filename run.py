9ffe9867f356c30ffd163d5423feef33d4f305dd
Sirjoy01 <chamed850@gmail.com>
2025-04-17 02:27:07 +0200

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,9 @@ from database import init_db
 app = Flask(__name__)
 init_db()
 
+@app.route("/")
+def index():
+    return "L'app fonctionne bien sur Rendre !"
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-
