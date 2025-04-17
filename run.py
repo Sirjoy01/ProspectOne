88c35a079044a2c9fbed89c30a827450423c22bd
Sirjoy01 <chamed850@gmail.com>
2025-04-17 14:56:45 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 from flask import Flask
-from routes import *
-from database import init_db
 
 app = Flask(__name__)
-app.secret_key = "ma_clé_secrète"
-init_db()
-    
+
+import routes
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
